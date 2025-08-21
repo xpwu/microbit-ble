02fbd88cfa1c1cd3522c818362f551e9f6d1bb1d
xpwu <diodexp@foxmail.com>
2025-08-21 17:14:30 +0800

    static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 启用静态导出
+  output: 'export',
+
+  // 确保路由正确处理
+  trailingSlash: false,
+
+  // 静态导出时图片需要取消优化
+  images: {
+    unoptimized: true
+  },
 };
 
 export default nextConfig;
