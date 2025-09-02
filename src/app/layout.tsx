e6acfb18cd64def53830e61a871d06a11b30f3af
xpwu <diodexp@foxmail.com>
2025-09-02 23:38:30 +0800

    <StrictMode>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {BlueControl} from "@/app/blueControl"
+import {StrictMode} from "react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+			<StrictMode>
 				<div className="fixed top-1 left-1 z-50 bg-white/50 p-1 shadow-lg h-fit w-fit">
 					<BlueControl />
 				</div>
         {children}
+			</StrictMode>
       </body>
     </html>
   );
