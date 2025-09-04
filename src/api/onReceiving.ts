3595ef0d23bd4b94e2f05d11bfbc56fd0ba6d497
xpwu <diodexp@foxmail.com>
2025-09-04 13:42:07 +0800

    Update onReceiving.ts

--- src/api/onReceiving.ts
+++ src/api/onReceiving.ts
@@ -4,6 +4,13 @@ import {DataLogEvent, PushData} from "@/table/datalog"
 import {Millisecond} from "ts-xutils"
 import {AllLogEvent, PushAllLog, Type} from "@/table/alllog"
 
+export function onReceiving(log: string) {
+	let lines = chunkDataIntoLines(log)
+	for (const line of lines) {
+		processOneLine(line)
+	}
+}
+
 function tryDataLog(log: string): boolean {
 	let colonIndex = log.indexOf(":")
 	if (colonIndex == -1 || colonIndex == log.length - 1 || colonIndex == 0) {
@@ -23,7 +30,33 @@ function tryDataLog(log: string): boolean {
 	return true;
 }
 
-export function onReceiving(log: string) {
+const MaxURATInputDataLength = 255
+
+function chunkDataIntoLines(data: string): string[] {
+	let lines: string[] = []
+	let buffer = ""
+	for (let i = 0; i < data.length; ++i) {
+		const ch = data[i]
+		buffer += ch
+		if (ch !== "\n" && buffer.length < MaxURATInputDataLength) {
+			continue
+		}
+		if (ch === "\n") {
+			// remove trailing white space
+			buffer = buffer.replace(/\s+$/, '');
+			// if anything remaining...
+			if (buffer.length) {
+				lines.push(buffer)
+			}
+		} else {
+			lines.push(buffer)
+		}
+		buffer = ""
+	}
+	return lines
+}
+
+function processOneLine(log: string) {
 	PushAllLog(log, Type.MicrobitLog)
 	Nc.post(new AllLogEvent).then()
 
@@ -37,3 +70,4 @@ export function onReceiving(log: string) {
 		Nc.post(new CmdLogEvent()).then()
 	}
 }
+
