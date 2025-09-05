a381b0cb3bf4efa0cf34de63fde6731baed051bf
xpwu <diodexp@foxmail.com>
2025-09-06 01:44:15 +0800

    update blue control style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,13 @@ export default function Home() {
 				<DataLog />
 			</div>
 			<div className="flex w-full h-1/2 pb-2">
-				<div className="relative w-1/2 h-full pb-2 border overflow-auto pl-2">
+				<div className="relative w-1/2 h-full pb-2 border pl-2">
 					<div className="absolute top-2 right-2 z-50 rounded-3xl py-2 hover:border-blue-700 border-gray-300 border shadow-lg">
 						<BlueControl />
 					</div>
-					<AllLogs />
+					<div className="w-full h-full overflow-auto">
+						<AllLogs />
+					</div>
 				</div>
 
 				<div className="flex flex-col w-1/2 h-full">
