b4bc0051f9e7ee7bbd1040b0083628bf1921db3c
xpwu <diodexp@foxmail.com>
2025-09-06 00:28:55 +0800

    Update sender.tsx

--- src/app/sender.tsx
+++ src/app/sender.tsx
@@ -50,7 +50,7 @@ export default function Sender() {
 			<input name="cmd" type="text" placeholder="input command"
 						 value={input} onChange={e => setInput(e.target.value)}
 						 className="flex-1 h-full px-4 border border-gray-300 rounded-l-lg
-						 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+						 focus:outline-none focus:ring-1 focus:ring-blue-700"/>
 
 			<label className="h-full bg-white border-y border-r border-gray-300 px-4 flex items-center justify-between text-gray-800">
 				以
@@ -62,7 +62,7 @@ export default function Sender() {
 				结束
 			</label>
 
-			<button className="h-full bg-blue-600 text-white px-2 rounded-r-lg hover:bg-blue-700 transition-colors flex-shrink-0"
+			<button className="h-full border border-blue-300 text-blue-500 px-2 rounded-r-lg hover:border-blue-700 hover:text-blue-700 transition-colors flex-shrink-0"
 							type="submit">
 				发送
 			</button>
