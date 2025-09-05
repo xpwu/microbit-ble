f80b830d45dd946ad8fed14f89896ad8ce6c39ee
xpwu <diodexp@foxmail.com>
2025-09-06 01:14:15 +0800

    modify css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 			</div>
 			<div className="flex w-full h-1/2 pb-2">
 				<div className="relative w-1/2 h-full pb-2 border overflow-auto pl-2">
-					<div className="absolute top-2 right-2 z-50 rounded-3xl py-2 hover:border-blue-700 border-blue-300 border shadow-lg">
+					<div className="absolute top-2 right-2 z-50 rounded-3xl py-2 hover:border-blue-700 border-gray-300 border shadow-lg">
 						<BlueControl />
 					</div>
 					<AllLogs />
