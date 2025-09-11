afd5973bfb9c243fabdc5aff5b2f2e93fd7c2a15
xpwu <diodexp@foxmail.com>
2025-09-11 18:29:16 +0800

    coding for sending

--- src/app/sender.tsx
+++ src/app/sender.tsx
@@ -4,6 +4,10 @@ import {FormEvent, useState} from "react"
 import {currentMicrobit} from "@/table/microbit"
 import {CmdLogEvent, LogType, PushCmdLog} from "@/table/cmdlog"
 import {Nc} from "@/nc"
+import cn from "classnames";
+import {Delay, Second} from "ts-xutils";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons";
 
 async function sendToMicrobit(cmd: string): Promise<Error|null> {
 	let res = await currentMicrobit()?.send(cmd)
@@ -42,22 +46,47 @@ async function send(e: FormEvent<HTMLFormElement>) {
 
 export default function Sender() {
 	const [input, setInput] = useState("")
+	const [sending, setSending] = useState(true)
 
 	return (
 		<form className = "flex h-12" onSubmit={async e => {
-			/* todo: disable*/
+			setSending(true)
 			await send(e)
-			/* todo: enable*/
+			await Delay(3*Second)
+			setSending(false)
 			setInput("");
 		}}>
-			<input name="cmd" type="text" placeholder="input command"
-						 value={input} onChange={e => setInput(e.target.value)}
-						 className="flex-1 h-full px-4 border border-gray-300 rounded-l-lg
-						 focus:outline-none focus:ring-1 focus:ring-blue-700"/>
 
-			<label className="h-full bg-white border-y border-r border-gray-300 px-4 flex items-center justify-between text-gray-800">
+			<input name="cmd" type="text" placeholder="input command" disabled={sending}
+					 value={input} onChange={e => setInput(e.target.value)}
+					 className={cn("flex-1 h-full px-4 border rounded-l-lg ",
+						 "focus:outline-none focus:ring-1 focus:ring-blue-700",
+						 {
+							 "border-gray-700": !sending,
+							 "border-gray-300": sending,
+							 "hover:not-focus:border-blue-700": !sending,
+							 "text-gray-800": !sending,
+							 "text-gray-300": sending,
+						 },
+						 "border-r-gray-300")}/>
+
+			<label className={cn("h-full bg-white border-y ",
+				"border-gray-300 px-4 flex items-center justify-between",
+				{
+					"border-gray-700": !sending,
+					"border-gray-300": sending,
+					"text-gray-800": !sending,
+					"text-gray-300": sending,
+				})}>
 				以
-				<select name="endFlag" className="border">
+				<select name="endFlag" disabled={sending}
+								className={cn("border",
+									{
+										"border-gray-300": sending,
+										"text-gray-300": sending,
+										"hover:not-focus:border-blue-700": !sending,
+									})}
+				>
 					<option value="#">#</option>
 					<option value=";">;</option>
 					<option value="/">/</option>
@@ -65,9 +94,23 @@ export default function Sender() {
 				结束
 			</label>
 
-			<button className="h-full border border-blue-300 text-blue-500 px-2 rounded-r-lg hover:border-blue-700 hover:text-blue-700 transition-colors flex-shrink-0"
-							type="submit">
+			<button type="submit" disabled={sending}
+							className={cn("relative h-full", "border", "px-2"
+								, "rounded-r-lg", "transition-colors", "flex-shrink-0",
+								{
+									"border-gray-700": !sending,
+									"border-gray-300": sending,
+									"text-blue-500": !sending,
+									"text-gray-300": sending,
+									"hover:border-blue-700": !sending,
+									"hover:text-blue-700": !sending,
+								},
+								"border-l-gray-300")}
+			>
 				发送
+				<div className={cn("absolute bottom-1/2 right-1/2 z-50 my-auto text-gray-700")} hidden={!sending}>
+					<FontAwesomeIcon icon={faSpinner} spinPulse size="lg"/>
+				</div>
 			</button>
 
 		</form>
