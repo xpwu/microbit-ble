14d8280d2d4dc6f47bcf84ac0a04ab47eb05e799
xpwu <diodexp@foxmail.com>
2025-09-13 03:06:28 +0800

    coding for load more 'all log', not debug

--- src/app/alllog.tsx
+++ src/app/alllog.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import {useEffect, useMemo, useRef, useState} from "react"
+import {useCallback, useEffect, useMemo, useRef, useState} from "react"
 import {Nc} from "@/nc"
-import {AllLogEvent, AllLogFrom, AllLogLast, Log, Type} from "@/table/alllog"
-import {Millisecond} from "ts-xutils"
+import {AllLogEvent, Last, LoadFrom, LoadUntil, Log, Type} from "@/table/alllog"
+import {Delay, Millisecond} from "ts-xutils"
 import {useInView} from "react-intersection-observer"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faAnglesDown, faSpinner} from "@fortawesome/free-solid-svg-icons"
+import cn from "classnames"
 
 function timeFormatter(date: Date) {
 	function pad2(number: number) { return (number < 10 ? '0' : '') + number }
@@ -17,35 +20,186 @@ function timeFormatter(date: Date) {
 const page = 30
 
 enum ShowState {
-	Selection, Continuous
+	ManualScrolling, AutoScrolling
+}
+
+enum MoreState {
+	Loading, NoMore, HasMore
 }
 
 export default function AllLogs() {
-	const initData = useMemo(()=>AllLogLast(2*page), [])
-	const indexRef = useRef({first: initData.lastIndex+1 - initData.logs.length, end: initData.lastIndex+1})
+	const initData = useMemo(()=>Last(2*page), [])
+	// [first, end) 来表示 logs 与底层数据之间的对应关系
+	const indexRef = useRef({first: initData.endIndex - initData.logs.length, end: initData.endIndex})
 	const [logs, setLogs] = useState<{key:number, val:Log}[]>(initData.logs.map(
 		(v, i)=>{return {key: indexRef.current.first + i, val: v}}))
-	const showStateRef = useRef(ShowState.Continuous)
+	const logsLenRef = useRef(logs.length)
+
+	const showStateRef = useRef(ShowState.AutoScrolling)
+
+	const hasMorePageRef = useRef({pre: initData.logs.length === 2*page, next: false})
+	const [headerState, setHeaderState] = useState(hasMorePageRef.current.pre? MoreState.HasMore: MoreState.NoMore)
+	const [footerState, setFooterState] = useState(hasMorePageRef.current.next? MoreState.HasMore: MoreState.NoMore)
+
+	const containerNodeRef = useRef<HTMLDivElement>(null)
+
+	const {ref: observerPreFlagNode, inView: preFlagInView} = useInView({initialInView: true})
+	const {ref: observerNextFlagNode, inView: nextFlagInView} = useInView({initialInView: true})
+
+	const loadPrePage = useCallback(async (ignoreScroll: boolean)=>{
+		if (containerNodeRef.current === null) {
+			return
+		}
+		const scrollTop = containerNodeRef.current.scrollTop
+		if (!ignoreScroll) {
+			await Delay(500*Millisecond)
+			if (containerNodeRef.current.scrollTop > scrollTop) {
+				return
+			}
+		}
+
+		const first = indexRef.current.first
+		setHeaderState(MoreState.Loading)
+		const newLogs = LoadUntil(first, page)
+		if (newLogs.length === 0) {
+			setHeaderState(MoreState.NoMore)
+			return
+		}
+		setHeaderState(MoreState.HasMore)
+		setLogs(logs => {
+			if (first != indexRef.current.first) {
+				return logs
+			}
+			if (!ignoreScroll) {
+				if (containerNodeRef.current === null || containerNodeRef.current.scrollTop > scrollTop) {
+					return logs
+				}
+			}
+
+			if (newLogs.length < page) {
+				setHeaderState(MoreState.NoMore)
+			}
+			const added = newLogs.map((v, i) => {
+				return {key: indexRef.current.first - newLogs.length + i, val: v}
+			})
+
+			indexRef.current.first -= newLogs.length
+			const res = added.concat(logs.slice(0, 4 * page - newLogs.length))
+			indexRef.current.end = res.at(-1)!.key + 1
+
+			return res
+		})
+	}, [])
+
+	const loadNextPage = useCallback(async (ignoreScroll: boolean)=>{
+		if (containerNodeRef.current === null) {
+			return
+		}
+		const scrollTop = containerNodeRef.current.scrollTop
+		if (!ignoreScroll) {
+			await Delay(500*Millisecond)
+			if (containerNodeRef.current.scrollTop < scrollTop) {
+				return
+			}
+		}
+
+		const end = indexRef.current.end
+		setFooterState(MoreState.Loading)
+		const newLogs = LoadFrom(end, page)
+		if (newLogs.length === 0) {
+			setFooterState(MoreState.NoMore)
+			return
+		}
+		setFooterState(MoreState.HasMore)
+		setLogs(logs => {
+			if (end != indexRef.current.end) {
+				return logs
+			}
+			if (!ignoreScroll) {
+				if (containerNodeRef.current === null || containerNodeRef.current.scrollTop < scrollTop) {
+					return logs
+				}
+			}
+
+			if (newLogs.length < page) {
+				setFooterState(MoreState.NoMore)
+			}
+			const added = newLogs.map((v, i) => {
+				return {key: indexRef.current.end + i, val: v}
+			})
+
+			indexRef.current.end += newLogs.length
+			let sliceStart = logs.length - (4*page - newLogs.length)
+			sliceStart = sliceStart < 0 ? 0 : sliceStart
+			const res = logs.slice(sliceStart).concat(added)
+			indexRef.current.first = res[0].key
+
+			return res
+		})
+	}, [])
+
+	useEffect(()=>{
+		logsLenRef.current = logs.length
+	}, [logs])
+
+	useEffect(()=>{
+		if (!hasMorePageRef.current.pre  || !preFlagInView) {
+			return
+		}
+		loadPrePage(false).then()
+	}, [preFlagInView, loadPrePage])
+
+	useEffect(()=>{
+		if (!hasMorePageRef.current.next || !nextFlagInView) {
+			return
+		}
+		loadNextPage(false).then()
+	}, [nextFlagInView, loadNextPage])
 
 	useEffect(()=>{
 		const item =Nc.addEvent(AllLogEvent, ()=>{
-			// if (showStateRef.current === ShowState.Selection) {
-			// 	return
-			// }
+			if (showStateRef.current === ShowState.ManualScrolling && logsLenRef.current >= 4*page) {
+				setFooterState(MoreState.HasMore)
+				return
+			}
 
-			const newLogs = AllLogFrom(indexRef.current.end).map(
-				(v,i)=>{return {key: indexRef.current.end+i, val: v}})
-			indexRef.current.end += newLogs.length
+			const end = indexRef.current.end
+			const newLogs = LoadFrom(end)
+			if (newLogs.length === 0) {
+				return
+			}
+
 			setLogs(logs =>{
-				let slice: {key:number, val:Log}[]
-				if (logs.length > 100) {
-					const dropData = 30
-					slice = logs.slice(dropData)
-					indexRef.current.first += dropData
-				} else {
-					slice = logs
+				setFooterState(MoreState.NoMore)
+
+				if (end != indexRef.current.end) {
+					return logs
 				}
-				return slice.concat(newLogs)
+
+				const added = newLogs.map((v, i) => {
+					return {key: indexRef.current.end + i, val: v}
+				})
+
+				let res = logs.concat(added)
+
+				if (showStateRef.current === ShowState.AutoScrolling) {
+					indexRef.current.end += newLogs.length
+					let sliceStart = res.length - 2*page
+					sliceStart = sliceStart < 0 ? 0 : sliceStart
+					res = res.slice(sliceStart)
+					indexRef.current.first = res[0].key
+
+					return res
+				}
+
+				if (res.length > 4*page) {
+					setFooterState(MoreState.HasMore)
+				}
+				res = res.slice(0, 4*page)
+				indexRef.current.first = res[0].key
+				indexRef.current.end = res.at(-1)!.key + 1
+
+				return res
 			})
 		})
 		return ()=>{
@@ -54,24 +208,55 @@ export default function AllLogs() {
 	}, [])
 
 	// state and auto-scroll
-	const {ref: observerLastNode, inView: lastNodeInView} = useInView({threshold: 0.1})
+	const {ref: observerLastNode, inView: lastNodeInView} = useInView({threshold: 0.1, initialInView:true})
 	const lastNodeRef = useRef<HTMLParagraphElement>(null)
 	useEffect(()=>{
-		if (showStateRef.current === ShowState.Continuous) {
+		if (showStateRef.current === ShowState.AutoScrolling) {
 			lastNodeRef.current?.scrollIntoView({behavior:"instant"})
 		}
 		observerLastNode(lastNodeRef.current)
 	}, [logs])
 	useEffect(()=>{
-		showStateRef.current = lastNodeInView?ShowState.Continuous:ShowState.Selection
+		showStateRef.current = lastNodeInView?ShowState.AutoScrolling:ShowState.ManualScrolling
 	}, [lastNodeInView])
 
+	const last = useCallback(async() => {
+		showStateRef.current = ShowState.AutoScrolling
+		const newLogs = Last(2*page)
+		setLogs(() => {
+			indexRef.current = {first: newLogs.endIndex - newLogs.logs.length, end: newLogs.endIndex}
+			setFooterState(MoreState.NoMore)
+			return newLogs.logs.map((v, i)=>{return {key: indexRef.current.first + i, val: v}})
+		})
+	}, [])
+
 	return (
-		<div className="w-full h-full overflow-y-auto wrap-break-word">
-			{logs.map(v=> {
+		<div className="relative w-full h-full overflow-y-auto wrap-break-word" ref={containerNodeRef}>
+			<button className={cn('my-1 mx-auto w-fit text-gray-600 border '
+				, ' rounded-lg hover:border-blue-300 text-[12px] p-0'
+				, (headerState != MoreState.HasMore? "hidden": "block"))}
+							onClick={()=>loadPrePage(true)}>加载更多</button>
+			<div className={cn('my-1 mx-auto w-fit text-gray-400 text-[12px]'
+				, (headerState != MoreState.NoMore? "hidden": "block"))}>------到顶了------</div>
+			<FontAwesomeIcon icon={faSpinner} spinPulse size="xs"
+											 style={{display: headerState != MoreState.Loading? "none": "block"}}
+											 className={'mx-auto my-1 text-gray-400'}/>
+
+			{logs.map((v, i)=> {
 				const time = timeFormatter(new Date(v.val.since1970/Millisecond))
+				const nextMoreIndex = showStateRef.current === ShowState.AutoScrolling ? logs.length : logs.length - page
+				const preMoreIndex = page
 				return (
-					<p key={v.key} ref={lastNodeRef}>
+					<p key={v.key}
+						 ref={node => {
+							 lastNodeRef.current = node
+							 if (i === nextMoreIndex) {
+								 observerNextFlagNode(node)
+							 }
+							 if (i === preMoreIndex) {
+								 observerPreFlagNode(node)
+							 }
+						 }}>
 						{v.val.type === Type.MicrobitLog ?
 							<>
 								<span className='text-gray-300'>{time}&nbsp;{'>'}&nbsp;</span>
@@ -83,6 +268,18 @@ export default function AllLogs() {
 						}
 				</p>)
 			})}
+
+			<button className={cn('my-1 mx-auto w-fit text-gray-600 border '
+				, ' rounded-lg hover:border-blue-300 text-[12px] p-0 block'
+				, (footerState != MoreState.HasMore? "hidden": "block"))}
+							onClick={()=>loadNextPage(true)}>加载更多</button>
+			<FontAwesomeIcon icon={faSpinner} spinPulse size="xs"
+											 style={{display: footerState != MoreState.Loading? "none": "block"}}
+											 className={'mx-auto my-1 text-gray-400'}/>
+
+			<FontAwesomeIcon icon={faAnglesDown} size="xs" onClick={last}
+											 style={{display: footerState != MoreState.HasMore? "none": "block"}}
+											 className={'absolute right-5 bottom-2 text-blue-300 hover:text-blue-500'}/>
 		</div>
 	)
 }
--- src/table/alllog.ts
+++ src/table/alllog.ts
@@ -16,13 +16,30 @@ export interface Log {
 
 const allLogs:Log[] = []
 
-// lastIndex: logs 最后一个元素在原数组对应的 index
-export function AllLogLast(lastCnt: number = 10): {logs: Log[], lastIndex: number} {
-  return {logs: allLogs.slice(-1*lastCnt), lastIndex: allLogs.length-1}
+// endIndex: logs 最后一个元素的下一个元素在原数组对应的 index
+export function Last(lastCnt: number = 10): {logs: Log[], endIndex: number} {
+  return {logs: allLogs.slice(-1*lastCnt), endIndex: allLogs.length}
 }
 
-export function AllLogFrom(fromIndex: number) : Log[] {
-  return allLogs.slice(fromIndex)
+export function LoadFrom(fromIndex: number, length?: number): Log[] {
+	if (length === undefined) {
+		length = allLogs.length
+	}
+	if (length <= 0) {
+		return []
+	}
+  return allLogs.slice(fromIndex, fromIndex+length)
+}
+
+export function LoadUntil(endIndex: number, length: number): Log[] {
+	if (length <= 0) {
+		return []
+	}
+	let start = endIndex - length
+	if (start < 0) {
+		start = 0
+	}
+	return allLogs.slice(start, length)
 }
 
 export function PushAllLog(log: string, type: Type, since1970: Duration) {
