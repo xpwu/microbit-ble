3a371257a5f041f0465b64a953d309b150bf1775
xpwu <diodexp@foxmail.com>
2025-09-12 12:25:40 +0800

    optimized auto-scrolling of all log

--- src/app/alllog.tsx
+++ src/app/alllog.tsx
@@ -4,6 +4,7 @@ import {useEffect, useMemo, useRef, useState} from "react"
 import {Nc} from "@/nc"
 import {AllLogEvent, AllLogFrom, AllLogLast, Log, Type} from "@/table/alllog"
 import {Millisecond} from "ts-xutils"
+import {useInView} from "react-intersection-observer"
 
 function timeFormatter(date: Date) {
 	function pad2(number: number) { return (number < 10 ? '0' : '') + number }
@@ -13,21 +14,34 @@ function timeFormatter(date: Date) {
 		+ ':' + pad2(date.getSeconds()) + '.' + pad3(date.getMilliseconds());
 }
 
+const page = 30
+
+enum ShowState {
+	Selection, Continuous
+}
+
 export default function AllLogs() {
-	const initData = useMemo(AllLogLast, [])
-	const index = useRef({first: initData.lastIndex - initData.logs.length + 1, last: initData.lastIndex})
-	const [logs, setLogs] = useState(initData.logs)
+	const initData = useMemo(()=>AllLogLast(2*page), [])
+	const indexRef = useRef({first: initData.lastIndex+1 - initData.logs.length, end: initData.lastIndex+1})
+	const [logs, setLogs] = useState<{key:number, val:Log}[]>(initData.logs.map(
+		(v, i)=>{return {key: indexRef.current.first + i, val: v}}))
+	const showStateRef = useRef(ShowState.Continuous)
 
 	useEffect(()=>{
 		const item =Nc.addEvent(AllLogEvent, ()=>{
-			const newLogs = AllLogFrom(index.current.last + 1)
-			index.current.last += newLogs.length
+			// if (showStateRef.current === ShowState.Selection) {
+			// 	return
+			// }
+
+			const newLogs = AllLogFrom(indexRef.current.end).map(
+				(v,i)=>{return {key: indexRef.current.end+i, val: v}})
+			indexRef.current.end += newLogs.length
 			setLogs(logs =>{
-				let slice: Log[]
+				let slice: {key:number, val:Log}[]
 				if (logs.length > 100) {
 					const dropData = 30
 					slice = logs.slice(dropData)
-					index.current.first += dropData
+					indexRef.current.first += dropData
 				} else {
 					slice = logs
 				}
@@ -39,40 +53,36 @@ export default function AllLogs() {
 		}
 	}, [])
 
-	const autoScroll = useRef(true)
-
-	const endRef = useRef<HTMLDivElement>(null)
+	// state and auto-scroll
+	const {ref: observerLastNode, inView: lastNodeInView} = useInView({threshold: 0.1})
+	const lastNodeRef = useRef<HTMLParagraphElement>(null)
 	useEffect(()=>{
-		if (autoScroll.current) {
-			endRef.current?.scrollIntoView({behavior:"instant"})
+		if (showStateRef.current === ShowState.Continuous) {
+			lastNodeRef.current?.scrollIntoView({behavior:"instant"})
 		}
+		observerLastNode(lastNodeRef.current)
 	}, [logs])
+	useEffect(()=>{
+		showStateRef.current = lastNodeInView?ShowState.Continuous:ShowState.Selection
+	}, [lastNodeInView])
 
 	return (
-		<div className="w-full h-full overflow-y-auto wrap-break-word" ref={(node)=>{
-			if (node === null) {
-				return
-			}
-
-			node.onscroll = ()=>{
-				autoScroll.current = node.scrollHeight - node.offsetHeight - node.scrollTop < node.offsetHeight/3
-			}
-		}}>
-			{logs.map((v, i)=> {
-				const time = timeFormatter(new Date(v.since1970/Millisecond))
-				switch (v.type) {
-					case Type.Tips:
-						return <p key={index.current.first+i} className='text-gray-400'>
-							<span className='text-gray-300'>{time}&nbsp;&nbsp;&nbsp;&nbsp;</span>
-							<span className='text-gray-400'>{v.log}</span>
-						</p>
-					case Type.MicrobitLog:
-						return <p key={index.current.first+i}> <span className='text-gray-300'>{time + ' >'}</span> {v.log}</p>
-					default:
-						return <></>
-				}
+		<div className="w-full h-full overflow-y-auto wrap-break-word">
+			{logs.map(v=> {
+				const time = timeFormatter(new Date(v.val.since1970/Millisecond))
+				return (
+					<p key={v.key} ref={lastNodeRef}>
+						{v.val.type === Type.MicrobitLog ?
+							<>
+								<span className='text-gray-300'>{time}&nbsp;{'>'}&nbsp;</span>
+								<span className='text-gray-700'>{v.val.log}</span>
+							</> : <>
+								<span className='text-gray-300'>{time}&nbsp;&nbsp;&nbsp;&nbsp;</span>
+								<span className='text-gray-400'>{v.val.log}</span>
+							</>
+						}
+				</p>)
 			})}
-			<div ref={endRef}></div>
 		</div>
 	)
 }
